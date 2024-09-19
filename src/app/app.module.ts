e215814bae030af5d70bcaa3cdc81e324903cc24
criticalRobin <matiastite1@gmail.com>
2024-09-19 15:26:56 -0500

    Deleted deprecated imports from app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PageListComponent } from './followUp/page-list/page-list.component';
-import { FormComponent } from './followUp/form/form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PageListComponent,
-    FormComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
