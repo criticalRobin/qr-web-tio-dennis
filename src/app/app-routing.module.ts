b8d4e4b20552ac14cdc0b73c462d4145a81b3bea
criticalRobin <matiastite1@gmail.com>
2024-09-19 15:19:50 -0500

    Created base routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,49 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './core/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+  },
+  {
+    path: 'agency',
+    loadChildren: () =>
+      import('./agency/agency.module').then((m) => m.AgencyModule),
+  },
+  {
+    path: 'channel',
+    loadChildren: () =>
+      import('./channel/channel.module').then((m) => m.ChannelModule),
+  },
+  {
+    path: 'client',
+    loadChildren: () =>
+      import('./client/client.module').then((m) => m.ClientModule),
+  },
+  {
+    path: 'employee',
+    loadChildren: () =>
+      import('./employee/employee.module').then((m) => m.EmployeeModule),
+  },
+  {
+    path: 'follow-up',
+    loadChildren: () =>
+      import('./follow-up/follow-up.module').then((m) => m.FollowUpModule),
+  },
+  {
+    path: 'qr',
+    loadChildren: () => import('./qr/qr.module').then((m) => m.QrModule),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
