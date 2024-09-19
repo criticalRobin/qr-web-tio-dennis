d35a0e819a248b62d55190c85b7a87856efd1875
criticalRobin <matiastite1@gmail.com>
2024-09-19 16:28:23 -0500

    Code from class 1

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,25 +1,43 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { CoreRoutingModule } from './core-routing.module';
-import { FormComponent } from './form/form.component';
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
 import { PageLoginComponent } from './page-login/page-login.component';
 import { LoginComponent } from './login/login.component';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+const MATERIAL = [
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatTooltipModule,
+];
 
 @NgModule({
   declarations: [
-    FormComponent,
     HeaderComponent,
     MenuComponent,
     PageLoginComponent,
-    LoginComponent
+    LoginComponent,
   ],
-  imports: [
-    CommonModule,
-    CoreRoutingModule
-  ]
+  imports: [CommonModule, MATERIAL],
+  exports: [MATERIAL, HeaderComponent, MenuComponent, PageLoginComponent],
 })
-export class CoreModule { }
+export class CoreModule {}
--- src/app/core/menu/menu.component.ts
+++ src/app/core/menu/menu.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'qr-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent {
-
+  protected menuList: string[] = [];
+  protected isExpanded: boolean = true;
 }
